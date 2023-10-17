4b7f570c17c95281eae126909f596e7075f65298
Easyvac16 <gdima9051@gmail.com>
2023-10-17 20:32:16 +0300

    Add project files.

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 29);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(304, 27);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 1;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(330, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 15);
+            label1.TabIndex = 2;
+            label1.Text = "UpperBound";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 15);
+            label2.TabIndex = 3;
+            label2.Text = "LowerBound";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 83);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(329, 83);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Stop";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(215, 29);
+            label3.Name = "label3";
+            label3.Size = new Size(13, 15);
+            label3.TabIndex = 6;
+            label3.Text = "0";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Prime", "Fibonacci" });
+            comboBox1.Location = new Point(174, 82);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(68, 23);
+            comboBox1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(93, 83);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 8;
+            button3.Text = "Continue";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(248, 82);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 9;
+            button4.Text = "Pause";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(414, 116);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(comboBox1);
+            Controls.Add(label3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private Label label3;
+        private ComboBox comboBox1;
+        private Button button3;
+        private Button button4;
+    }
+}
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,223 @@
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private bool continueGenerating = true;
+        private bool genFibonacci = true;
+        private bool isPaused = false;
+        private int lowerBound;
+        private int upperBound;
+        private int currentNumber;
+        Thread generatorThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lowerBound = 2;
+            upperBound = int.MaxValue;
+            currentNumber = lowerBound;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void GeneratePrimeNumbers()
+        {
+            while (continueGenerating)
+            {
+                if (!isPaused)
+                {
+                    if (IsPrime(currentNumber))
+                    {
+                        UpdateLabel(currentNumber);
+                    }
+                    currentNumber++;
+                    Thread.Sleep(100);
+
+                    if (currentNumber > upperBound)
+                    {
+                        continueGenerating = false;
+                        EnabledDisabled();
+                    }
+                }
+            }
+        }
+
+        private void GenerateFibonacci()
+        {
+            long a = 0;
+            long b = 1;
+
+            while (genFibonacci)
+            {
+                if (!isPaused)
+                {
+                    long temp = a;
+                    a = b;
+                    b = temp + b;
+
+                    if (b > upperBound)
+                    {
+                        genFibonacci = false;
+                        EnabledDisabled();
+                        break;
+                    }
+
+                    if (IsFibonacci(b))
+                    {
+                        UpdateLabel((int)b);
+                    }
+
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+
+        private void UpdateLabel(int number)
+        {
+            if (label3.InvokeRequired)
+            {
+                label3.Invoke((MethodInvoker)(() => label3.Text = number.ToString()));
+            }
+            else
+            {
+                label3.Text = number.ToString();
+            }
+        }
+
+        private bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private bool IsFibonacci(long number)
+        {
+            if (number <= 1)
+                return false;
+
+            long a = 0;
+            long b = 1;
+
+            while (b < number)
+            {
+                long temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            return b == number;
+        }
+
+        private void EnabledDisabled()
+        {
+            if (comboBox1.InvokeRequired)
+            {
+                comboBox1.Invoke((MethodInvoker)(() => comboBox1.Enabled = true));
+            }
+            if (button1.InvokeRequired)
+            {
+                button1.Invoke((MethodInvoker)(() => button1.Enabled = true));
+            }
+            if (button2.InvokeRequired)
+            {
+                button2.Invoke((MethodInvoker)(() => button2.Enabled = false));
+            }
+            if (button3.InvokeRequired)
+            {
+                button3.Invoke((MethodInvoker)(() => button3.Enabled = false));
+            }
+            if (button4.InvokeRequired)
+            {
+                button4.Invoke((MethodInvoker)(() => button4.Enabled = false));
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+
+            currentNumber = lowerBound;
+
+            if (comboBox1.SelectedIndex == 0)
+            {
+                genFibonacci = false;
+                continueGenerating = true;
+                generatorThread = new Thread(GeneratePrimeNumbers);
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                continueGenerating = false;
+                genFibonacci = true;
+                generatorThread = new Thread(GenerateFibonacci);
+            }
+
+            generatorThread.Start();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            continueGenerating = false;
+            genFibonacci = false;
+
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            comboBox1.Enabled = true;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                upperBound = int.Parse(textBox2.Text);
+            }
+            else if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                upperBound = int.MaxValue;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                lowerBound = int.Parse(textBox1.Text);
+            }
+            else if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                lowerBound = 2;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            isPaused = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            isPaused = true;
+        }
+
+    }
+}
